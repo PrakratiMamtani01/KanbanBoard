4905f5450713dbe39a20c922095a4c3b84faaf82
PrakratiMamtani01 <138221079+PrakratiMamtani01@users.noreply.github.com>
2024-08-27 10:09:39 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
+// useRoutes hook is used to render the appropriate UI based on the current URL.
+import { useRoutes } from "react-router";
+// this is a folder creates by us that contains the configuration of the root
+import routes from "./routes";
 
+function App() {
+  // we check the configurationa and return element
+	const element = useRoutes(routes);
+	return <>{element}</>;
+}
+
+export default App;
